d05bdc602ea0d61315e3d93058d48c7bc695ee9c
okorie <wisdomokorie18@gmail.com>
2024-11-09 14:24:33 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        customBlue: '#30277F',
+        C5C6C9: '#C5C6C9',
+      },
+       maxWidth: {
+        'custom': '546px',
+        'custom5': '500px',
+      },
+    },
+  },
+  plugins: [],
+}
